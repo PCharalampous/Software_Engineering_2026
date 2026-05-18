addf22af6fbb2ab8e021adcca9f9b156df076e61
Odysseas <odysseasbalatsouras@gmail.com>
2026-05-18 15:29:42 +0300

    ui screens updated

--- ProjectCode/src/usecase1/HomeScreenLeftPanel.java
+++ ProjectCode/src/usecase1/HomeScreenLeftPanel.java
@@ -2,14 +2,17 @@ package usecase1;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class HomeScreenLeftPanel {
 	
 	private Label leftBoxLabel, headerLbl;
-	private VBox leftBox;
+	private VBox leftBox, joinRoom;
 	private Separator separator;
 	
 	HomeScreenLeftPanel(){
@@ -17,23 +20,47 @@ public class HomeScreenLeftPanel {
 		separator = new Separator();
         leftBoxLabel = new Label("Not in room yet");
         leftBox = new VBox(10);
-        leftBox.getChildren().addAll(headerLbl ,separator ,leftBoxLabel);
+        
+        joinRoom = joinRoomComponent();
+        leftBox.getChildren().addAll(headerLbl ,separator ,leftBoxLabel,joinRoom);
+        
 		leftBox.setAlignment(Pos.TOP_CENTER);
         leftBox.setPadding(new Insets(10));
         leftBox.setPrefWidth(200);
 	}
 	
 	
-	private void leftPanelStyling(VBox obj) {
+	private VBox joinRoomComponent() {
+		VBox joinRoom = new VBox(10);
+		Label joinRoomLbl = new Label("JOIN ROOM");
+		TextField roomIdSect = new TextField("Enter room id...");
+		Button joinBtn = new Button("JOIN");
+//		Separator joinRoomSeparator = new Separator();
+		joinRoom.getChildren().addAll(joinRoomLbl,roomIdSect,joinBtn);
+		joinRoom.setAlignment(Pos.CENTER);
+
+		return joinRoom;
+	}
+	
+	private void leftPanelStyling(VBox obj, VBox joinObj) {
 		obj.setStyle(
 			"-fx-background-color: #D9D9FF;"+
 	        "-fx-border-color: #0000FF;" +
 	        "-fx-border-width: 2;"
 	    );
+		
+		joinObj.setStyle(
+				"-fx-background-color: white;" +
+				"-fx-border-color: #0000FF;" +
+			    "-fx-border-width: 1;" +
+			    "-fx-border-radius: 12;" +
+			    "-fx-background-radius: 12;" +
+			    "-fx-padding: 20;"
+		);
 	}
 	
 	public VBox getLeftPanel() {
-		leftPanelStyling(this.leftBox);
+		leftPanelStyling(this.leftBox, this.joinRoom);
 		return this.leftBox;
 	} 
 }
