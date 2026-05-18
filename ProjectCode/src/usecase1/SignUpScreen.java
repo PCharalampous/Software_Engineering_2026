9616cbd97a666a44065cf8639edc1f1772f1cb9e
Odysseas <odysseasbalatsouras@gmail.com>
2026-05-19 02:44:18 +0300

    button partial functiability added

--- ProjectCode/src/usecase1/SignUpScreen.java
+++ ProjectCode/src/usecase1/SignUpScreen.java
@@ -3,6 +3,7 @@ package usecase1;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 //import javafx.scene.layout.StackPane;
@@ -14,11 +15,14 @@ public class SignUpScreen {
 	
 private Stage primaryStage;
 	
+	private Button createAcc;
+
 	public SignUpScreen(Stage stage) {
 		this.primaryStage = stage;
 	}
 	
 	public void createWindow() {
+		createAcc = new Button("Create Account");
         Label label = new Label("Create Account!");
 
         VBox root = new VBox(5);
@@ -33,23 +37,41 @@ private Stage primaryStage;
 
         
         root.setPadding(new Insets(5));
-        root.getChildren().addAll(
-        		
-        		label,
-                innerPanel.getCredenInput(),
-                innerPanel.getSignUpBtn()
-                
-        );        
+        root.getChildren().addAll(label, innerPanel.getCredenInput());        
         
         primaryStage.setResizable(false);
 
-        hbox.getChildren().addAll(innerPanel.getSignUpBtn());
+        hbox.getChildren().addAll(createAcc);
         hbox.setAlignment(Pos.CENTER);
         
         root.getChildren().add(hbox);
         root.setAlignment(Pos.CENTER);
+        
+        buttonStyling();
+        buttonsFunctiability();
         // Show window
         primaryStage.show();
         
     }
+	
+	private void buttonsFunctiability() {
+		
+		createAcc.setOnAction(e -> {
+				LogInScreen logINsc = new LogInScreen(this.primaryStage);
+				logINsc.createWindow();
+			}
+		);
+		
+	}
+	
+	private void buttonStyling() {
+		createAcc.setStyle(
+    		    "-fx-background-radius: 100; " +
+    		    "-fx-max-width: 1000px; " +
+    		    "-fx-max-height: 1000px; " +
+    		    "-fx-font-size: 15px;"
+    		);
+    }
+	
+	
 }
