0a879219a2198871e441c1d283a47d9cff6a41dc
Giannis Dimitriou Masgalas <149790907+idimitrioumasgalas@users.noreply.github.com>
2026-05-19 12:31:45 +0300

    Delete usecase10 directory

--- usecase10/src/Application.java
+++
@@ -1,4 +0,0 @@
-public class Application {
-    public String title, subtitle, status;
-    public Application(String t, String s, String st) { this.title = t; this.subtitle = s; this.status = st; }
-}
