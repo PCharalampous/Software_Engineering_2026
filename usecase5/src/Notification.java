5e13d92d59b3a1293d48932157bed512533aab1a
Giannis Dimitriou Masgalas <149790907+idimitrioumasgalas@users.noreply.github.com>
2026-05-19 12:31:54 +0300

    Delete usecase5 directory

--- usecase5/src/Notification.java
+++
@@ -1,8 +0,0 @@
-public class Notification {
-    public String category, text, detail, target, tagColor;
-    public boolean read;
-
-    public Notification(String cat, String txt, String det, String tgt, String tc) {
-        this.category = cat; this.text = txt; this.detail = det; this.target = tgt; this.tagColor = tc;
-    }
-}
