2e8e8f2f3feb862271fc05069dd32289d32c9daa
Giorgos Efthymiou <147935966+gefth@users.noreply.github.com>
2026-05-22 21:39:04 +0300

    Delete code_GIO/HomeIssueReportCode directory

--- code_GIO/HomeIssueReportCode/src/main/java/com/example/AppLauncher.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class AppLauncher {
-    public static void main(String[] args) {
-        // Redirects entry logic to bypass JavaFX module verification checks
-        MainApp.main(args);
-    }
-}
