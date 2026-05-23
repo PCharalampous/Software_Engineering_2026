3a15be3c2773fdffdcfacda81ea5cc4920c64b1d
Giannis Dimitriou Masgalas <149790907+idimitrioumasgalas@users.noreply.github.com>
2026-05-23 20:53:10 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -193,6 +193,7 @@ CREATE TABLE rewards (
     room_id INT NOT NULL,                           -- Ανταμοιβές ανά δωμάτιο/σπίτι
     reward_name VARCHAR(150) NOT NULL,              -- Όνομα ανταμοιβής
     cost INT NOT NULL DEFAULT 0,                    -- Κόστος σε πόντους
+    is_available BOOLEAN NOT NULL DEFAULT TRUE,     -- Διαθεσιμότητα για ψηφοφορία
     approve_votes INT NOT NULL DEFAULT 0,           -- Ψήφοι έγκρισης ολοκλήρωσης
     reject_votes INT NOT NULL DEFAULT 0,            -- Ψήφοι απόρριψης ολοκλήρωσης
     ui_color VARCHAR(20) NULL,                      -- Χρώμα για το UI αναπαράστασης
